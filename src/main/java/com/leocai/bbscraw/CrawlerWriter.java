22588aebc94962587b3266ae744b105d0f56015a
caiqingliang <caiqingliang@meituan.com>
2016-08-29 15:28:52 +0800

    优化

--- src/main/java/com/leocai/bbscraw/CrawlerWriter.java
+++ src/main/java/com/leocai/bbscraw/CrawlerWriter.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 /**
